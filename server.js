e60731dfa6da181cd873d45a3c28e9d11aa7acf3
Course Dashboard <osama.eldrieny@gmail.com>
2026-01-24 22:28:03 +0800

    Fix CORS to allow GitHub Pages frontend

--- server.js
+++ server.js
@@ -46,7 +46,17 @@ if (SENDER_APP_PASSWORD) {
 }
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: [
+        'https://osama-eldrieny.github.io',
+        'http://localhost:3000',
+        'http://localhost:8000',
+        'http://localhost:5173'
+    ],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(express.static(__dirname));
